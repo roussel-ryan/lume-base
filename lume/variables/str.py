e0878a063a0ccf58e963a33762693795cf2f8593
Christopher M. Pierce <contact@chris-pierce.com>
2026-03-24 12:58:59 -0700

    add str variable

--- lume/variables/str.py
+++ lume/variables/str.py
@@ -5,33 +5,16 @@ import warnings
 from lume.variables.variable import Variable, ConfigEnum
 
 
-class IntVariable(Variable):
-    """Variable for int values.
+class StrVariable(Variable):
+    """Variable for str values.
 
     Attributes
     ----------
-    default_value : int | None
+    default_value : str | None
         Default value for the variable.
-    value_range : tuple[int, int] | None
-        Validate variable is in range [value_range[0], value_range[1]] (inclusive). Ignore if set to `None`.
-    unit : str | None
-        Unit associated with the variable.
     """
 
-    default_value: int | None = None
-    value_range: tuple[int, int] | None = None
-    unit: str | None = None
-
-    @field_validator("value_range", mode="before")
-    @classmethod
-    def validate_value_range(cls, value):
-        if value is not None:
-            value = tuple(value)
-            if not value[0] <= value[1]:
-                raise ValueError(
-                    f"Minimum value ({value[0]}) must be lower or equal than maximum ({value[1]})."
-                )
-        return value
+    default_value: str | None = None
 
     @model_validator(mode="after")
     def validate_default_value(self):
@@ -77,24 +60,4 @@ class IntVariable(Variable):
 
     def _validate_value_is_within_range(self, value: float, config: ConfigEnum = None):
         config = self._validation_config_as_enum(config)
-
-        if not self._value_is_within_range(value):
-            error_message = (
-                "Value ({}) of '{}' is out of valid range: ([{},{}]).".format(
-                    value, self.name, *self.value_range
-                )
-            )
-            range_warning_message = (
-                error_message
-                + " Executing the model outside of the range may result in"
-                " unpredictable and invalid predictions."
-            )
-            if config == ConfigEnum.WARN:
-                warnings.warn(range_warning_message)
-            else:
-                raise ValueError(error_message)
-
-    def _value_is_within_range(self, value) -> bool:
-        if self.value_range is not None:
-            return self.value_range[0] <= value <= self.value_range[1]
-        return True
+        # Additional validation here

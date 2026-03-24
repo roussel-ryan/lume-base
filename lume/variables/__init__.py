02816ea1e40610bbe4af2826ecfe7f81085df6e6
Christopher M. Pierce <contact@chris-pierce.com>
2026-03-24 13:11:00 -0700

    add to __init__.py

--- lume/variables/__init__.py
+++ lume/variables/__init__.py
@@ -2,6 +2,9 @@ from lume.variables.scalar import ScalarVariable
 from lume.variables.variable import Variable, ConfigEnum
 from lume.variables.particle_group import ParticleGroupVariable
 from lume.variables.ndvariable import NDVariable
+from lume.variables.str import StrVariable
+from lume.variables.int import IntVariable
+from lume.variables.bool import BoolVariable
 
 __all__ = [
     "Variable",
@@ -9,4 +12,7 @@ __all__ = [
     "ScalarVariable",
     "ParticleGroupVariable",
     "NDVariable",
+    "StrVariable",
+    "IntVariable",
+    "BoolVariable",
 ]
